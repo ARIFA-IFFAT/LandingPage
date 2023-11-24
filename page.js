fa9a41492232cb68f66024900fcc318927177a06
ARIFA-IFFAT <134854300+ARIFA-IFFAT@users.noreply.github.com>
2023-11-25 00:33:01 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,22 @@
+
+'use client';
+
+import React from 'react'
+import Header from '@/Components/Header'
+import Features from '@/Components/Features'
+import Feature_SS from '@/Components/Feature_SS'
+import Discover from '@/Components/Discover'
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Features />
+      <Feature_SS/>
+      <Discover/>
+    </>
+    
+  )
+}
+
+export default page
